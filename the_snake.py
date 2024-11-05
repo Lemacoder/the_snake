eeb1b474d4940279d7ef71d5f2f11b986a25b8d7
Lemacoder <ladygin.anastasia@yandex.ru>
2024-11-05 14:25:33 +0300

    pep8

--- the_snake.py
+++ the_snake.py
@@ -43,7 +43,7 @@ class GameObject:
         self.body_color = body_color if body_color is not None else SNAKE_COLOR
 
     def draw(self):
-        """Метод для отрисовки объекта. 
+        """Метод для отрисовки объекта.
         Должен быть переопределен в дочерних классах."""
         pass
 
